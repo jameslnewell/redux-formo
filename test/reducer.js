c63d8dfd83b1597c23bf0cd8a19e7af96b4d2556
James Newell <james@digitaledgeit.com.au>
2015-10-26 07:56:52 +1100

    initial commit

---
+++ test/reducer.js
@@ -0,0 +1,54 @@
+import assert from 'assert';
+import * as actions from '../lib/actions';
+import * as creators from '../lib/creators';
+import {default as formReducer} from '../lib/reducer';
+
+describe('reducer', () => {
+
+  it('should return the same state when the action is targeted at another form');
+
+  it('should return an initial state when the state is undefined', () => {
+    let reducer = formReducer('about', ['name', 'phone']);
+    let state = reducer();
+
+    assert(!state.valid);
+
+    assert(!state.fields.name.valid);
+    assert.equal(state.fields.name.value, '');
+
+    assert(!state.fields.phone.valid);
+    assert.equal(state.fields.phone.value, '');
+
+  });
+
+  describe(actions.RESET_FORM, () => {
+
+    it('should return the initial state');
+
+  });
+
+  describe(actions.CHANGE_FORM, () => {
+
+    it('should update the value of the phone field without changing whether the field is valid', () => {
+      let reducer = formReducer('about', ['name', 'phone']);
+      let state = reducer(reducer(), creators.changeForm('about', 'phone', '0466'));
+
+      //shouldn't change the name field
+      assert(!state.fields.name.valid);
+      assert.equal(state.fields.name.value, '');
+
+      assert(!state.fields.phone.valid);//shouldn't change the valid status
+      assert.equal(state.fields.phone.value, '0466'); //should update the value
+
+    });
+
+  });
+
+  describe(actions.VALIDATE_FORM, () => {
+
+    it('be valid');
+    it('be invalid');
+
+  });
+
+});
