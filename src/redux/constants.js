77b75e80280d76aab1194e4640308e305d1cbbb2
James Newell <j.newell@nib.com.au>
2015-12-08 16:12:07 +1100

    update constants to be more unique

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,7 +1,7 @@
-export const FOCUS = '@@redux-formo/focus';
-export const BLUR = '@@redux-formo/blur';
-export const CHANGE = '@@redux-formo/change';
+export const FOCUS = '@redux-formo/focus';
+export const BLUR = '@redux-formo/blur';
+export const CHANGE = '@redux-formo/change';
 
-export const FILTER = '@@redux-formo/filter';
-export const VALIDATE = '@@redux-formo/validate';
-export const SUBMIT = '@@redux-formo/submit';
+export const FILTER = '@redux-formo/filter';
+export const VALIDATE = '@redux-formo/validate';
+export const SUBMIT = '@redux-formo/submit';
